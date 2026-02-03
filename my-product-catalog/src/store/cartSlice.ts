f728933d7af62f5826937ee53f8aa18e054fe192
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2026-02-03 14:02:01 +0100

    Defined the product interface and implementing Redux to handle the cart

---
+++ my-product-catalog/src/store/cartSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Product } from "../types/product";
+
+interface CartState {
+  items: Product[];
+}
+
+const initialState: CartState = { items: [] };
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<Product>) => {
+      state.items.push(action.payload);
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
